8c04303adc48df064fba882ef727c90698472844
steven-albanese <steven.albanese91@gmail.com>
2016-06-02 15:32:30 -0400

    updating the number of iterations

--- examples/AlchemicalLoopSoftening/AlchemicalLoopSoftening.py
+++ examples/AlchemicalLoopSoftening/AlchemicalLoopSoftening.py
@@ -152,5 +152,5 @@ if __name__ == '__main__':
     system.mcmc_sampler.nsteps = 500
     system.exen_sampler.locality = 10
     system.sams_sampler.update_method = 'optimal'
-    niterations = 5000
+    niterations = 10000
     system.sams_sampler.run(niterations)
