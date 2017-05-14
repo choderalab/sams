633d0cf9d747469102b9706004cf0e6573420e79
John Chodera <choderaj@mskcc.org>
2017-05-13 22:10:01 -0400

    Update DDR1 sampling example.

--- examples/ddr1-dfg-flip/dfg-flip-dihedral.py
+++ examples/ddr1-dfg-flip/dfg-flip-dihedral.py
@@ -32,16 +32,19 @@ state_pdb_filename = 'setup/state_DFG_IN.pdb'
 pdb_filename = 'setup/systems/Abl-STI/complex.pdb'
 
 # Specify umbrellas for distance restraint
-umbrella_sigma = 0.25 * unit.angstroms # umbrella stddev width in absence of external PMF (no Jacobian)
+umbrella_sigma = 5*unit.degrees # umbrella stddev width in absence of external PMF (no Jacobian)
 
-umbrella_atoms = [2324,2851]
-# ATOM   2325  CA  GLY A 150       5.033  49.691  31.673  1.00  0.00           C
-# ATOM   2852  HE2 PHE A 182       7.559  52.496  39.377  1.00  0.00           H
-min_distance = 2.0 * unit.angstroms
-max_distance = 30.0 * unit.angstroms
-distance_unit = unit.angstroms
-numbrellas = int((max_distance - min_distance) / umbrella_sigma + 2)
-umbrella_distances = np.linspace(min_distance/distance_unit, max_distance/distance_unit, numbrellas) * distance_unit
+umbrella_atoms = [2817, 2815, 2825, 2830] # atoms involved in umbrella restraint
+#ATOM   2818  CB  ALA A 180       1.927  52.416  41.379  1.00  0.00           C  
+#ATOM   2816  CA  ALA A 180       3.319  52.098  40.823  1.00  0.00           C  
+#ATOM   2826  CA  ASP A 181       5.071  50.442  43.834  1.00  0.00           C  
+#ATOM   2831  CG  ASP A 181       2.928  49.040  44.337  1.00  0.00           C  
+
+min_dihedral = -180*unit.degrees
+max_dihedral = +180*unit.degrees
+dihedral_unit = unit.degrees
+numbrellas = int((max_dihedral - min_dihedral) / umbrella_sigma + 2)
+umbrella_values = np.linspace(min_dihedral/dihedral_unit, max_dihedral/dihedral_unit, numbrellas) * dihedral_unit
 
 # Output SAMS filename
 netcdf_filename = 'output.nc'
@@ -93,12 +96,15 @@ else:
     pass
 
 # Add umbrella restraint with global variable to control umbrella position
-print('umbrella schedule between atoms %d and %d: %s' % (umbrella_atoms[0], umbrella_atoms[1], str(umbrella_distances)))
-energy_function = '(umbrella_K/2.0)*(r-umbrella_r0)^2'
-umbrella_force = openmm.CustomBondForce(energy_function)
-umbrella_force.addGlobalParameter('umbrella_K', 0.0) # spring constant
-umbrella_force.addGlobalParameter('umbrella_r0', 0.0) # umbrella distance
-umbrella_force.addBond(*umbrella_atoms, [])
+print('umbrella schedule for dihedral defined by atoms %s : %s' % (str(umbrella_atoms), str(umbrella_values)))
+
+from numpy import pi
+energy_function = '- (umbrella_K/2) * cos(min(dtheta, 2*pi-dtheta)); dtheta = abs(theta-umbrella_r0);'
+energy_function += 'pi = %f;' % pi
+umbrella_force = openmm.CustomTorsionForce(energy_function)
+umbrella_force.addGlobalParameter('umbrella_K', 0.0)
+umbrella_force.addGlobalParameter('umbrella_r0', 0.0)
+umbrella_force.addTorsion(*umbrella_atoms, [])
 umbrella_K = kT/umbrella_sigma**2
 system.addForce(umbrella_force)
 
@@ -113,9 +119,9 @@ thermodynamic_states.append( ThermodynamicState(system=system, temperature=tempe
 
 # Umbrella on state
 alchemical_lambda = 0.0
-for umbrella_distance in umbrella_distances:
+for umbrella_value in umbrella_values:
     parameters = {
-        'umbrella_K' : umbrella_K.value_in_unit_system(unit.md_unit_system), 'umbrella_r0' : umbrella_distance.value_in_unit_system(unit.md_unit_system), # umbrella parameters
+        'umbrella_K' : umbrella_K.value_in_unit_system(unit.md_unit_system), 'umbrella_r0' : umbrella_value.value_in_unit_system(unit.md_unit_system), # umbrella parameters
     }
     thermodynamic_states.append( ThermodynamicState(system=system, temperature=temperature, pressure=pressure, parameters=parameters) )
 
@@ -160,7 +166,7 @@ thermodynamic_state = thermodynamic_states[thermodynamic_state_index]
 sampler_state = SamplerState(positions=positions, box_vectors=box_vectors)
 mcmc_sampler = MCMCSampler(sampler_state=sampler_state, thermodynamic_state=thermodynamic_state, ncfile=ncfile, platform=platform)
 mcmc_sampler.timestep = timestep
-mcmc_sampler.nsteps = 500
+mcmc_sampler.nsteps = 1000
 #mcmc_sampler.pdbfile = open('output.pdb', 'w') # uncomment this if you want to write a PDB trajectory as you simulate; WARNING: LARGE!
 mcmc_sampler.topology = topology
 mcmc_sampler.verbose = True
